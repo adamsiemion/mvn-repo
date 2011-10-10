334fcfd1e787a664c721e694a5cc58c97cd580a8
adko-pl <adam.siemion+github@gmail.com>
2011-10-10 15:39:31 +0200

    added dep and repo project

---
+++ dep-project/src/main/java/com/adko/Dep.java
@@ -0,0 +1,9 @@
+package com.adko;
+
+import com.adko.Project;
+
+public class Dep {
+  public static void main(String[] args) {
+    Project.main(args);
+  }
+}
---
+++ repo-project/src/main/java/com/adko/Project.java
@@ -0,0 +1,7 @@
+package com.adko;
+
+public class Project {
+  public static void main(String[] args) {
+    System.out.println("Test project");
+  }
+}
