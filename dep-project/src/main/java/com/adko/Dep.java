4f7669223ae40c6bcf3f908c06b6794e79b28d0b
Adam Siemion <adam.siemion@gmail.com>
2014-06-02 23:10:21 +0200

    removed the current files

--- dep-project/src/main/java/com/adko/Dep.java
+++
@@ -1,9 +0,0 @@
-package com.adko;
-
-import com.adko.Project;
-
-public class Dep {
-  public static void main(String[] args) {
-    Project.main(args);
-  }
-}
--- repo-project/src/main/java/com/adko/Project.java
+++
@@ -1,7 +0,0 @@
-package com.adko;
-
-public class Project {
-  public static void main(String[] args) {
-    System.out.println("Test project");
-  }
-}
